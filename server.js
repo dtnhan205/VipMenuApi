62be732328bf1838005a217c36418d065fa68680
dinhthenhan <dinhthenhandako@gmail.com>
2025-06-27 01:25:39 +0700

    fix server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 const featureRouter = require('./routers/featureRouter');
 
 app.use(express.json());
-
 app.use('/api', featureRouter);
 
 app.listen(port, () => {
