1aca2b77306edae7ac0ff74da83f5dbd85de6fdd
dinhthenhan <dinhthenhandako@gmail.com>
2025-06-27 01:38:57 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const featureRouter = require('./routers/featureRouter');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors()); 
 app.use('/api', featureRouter);
 
 app.listen(port, () => {
